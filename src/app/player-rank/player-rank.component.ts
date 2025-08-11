ce0cdf2b0a7bc4557896e62bd0d1c1f4f9766ce4
andronic11 <sorineandronic@outlook.com>
2025-08-11 17:46:43 -0400

    Adding average points and player rank to table

--- src/app/player-rank/player-rank.component.ts
+++ src/app/player-rank/player-rank.component.ts
@@ -26,6 +26,6 @@ export class PlayerRankComponent implements OnInit {
       });
   }
 
-  tableHeaders = ['Name', 'Score', 'Position', 'Team']
+  tableHeaders = ['Rank', 'Name','Position', 'Team', 'Score', 'AVG',]
   
 }
